2ecf5a8177bef5284ca7c1c4fa785786e637e76c
so02s <so02s@mail.ru>
2025-01-22 20:18:17 +0300

    need to connect front with back

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 
 export DISPLAY=:0
 export XAUTHORITY=/home/user/.Xauthority
-#xhost +local:
 
-sudo .venv/bin/python3 main.py
+source /home/monitor/monitor_for_meeting_room/.venv/bin/activate
+pip install -r /home/monitor/monitor_for_meeting_room/requirements.txt
+deactivate
+
+# НЕ ЗАБЫТЬ - ПРИ ЗАПУСКЕ СКРИПТА ДОЛЖНЫ БЫТЬ ПРАВА SUDO
+sudo /home/monitor/monitor_for_meeting_room/.venv/bin/python3 /home/monitor/monitor_for_meeting_room/main.py
